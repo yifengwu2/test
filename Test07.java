ddfd4bc6993567c2db64b867d2a8f6521bf86eb5
yifengwu2 <2454495978@qq.com>
2025-11-30 21:08:00 +0800

    虚拟等待

--- Test07.java
+++ Test07.java
@@ -8,11 +8,12 @@ public class Test07 {
     static boolean hasCigarette = false;
     static boolean hasTakeout = false;
 
+    //虚假等待
     public static void main(String[] args) {
-        Thread t1 = new Thread(() -> {
+        new Thread(() -> {
             synchronized (room) {
                 log.debug("有烟没[{}]", hasCigarette);
-                if (!hasCigarette) {
+                while (!hasCigarette) {
                     log.debug("没烟休息会");
 //                    Sleeper(2);
                     try {
@@ -24,10 +25,31 @@ public class Test07 {
                 log.debug("有烟没[{}]", hasCigarette);
                 if (hasCigarette) {
                     log.debug("小南开始干活");
+                } else {
+                    log.debug("没干成活");
                 }
             }
-        }, "小南");
-        t1.start();
+        }, "小南").start();
+
+        new Thread(() -> {
+            log.debug("外卖到了吗[{}]", hasTakeout);
+            synchronized (room) {
+                if (!hasTakeout) {
+                    log.debug("等外卖中[{}]", hasTakeout);
+                    try {
+                        room.wait();
+                    } catch (InterruptedException e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+                    log.debug("外卖到了吗[{}]", hasTakeout);
+                    if (hasTakeout) {
+                        log.debug("小女开始吃外卖");
+                    } else {
+                        log.debug("没吃成外卖");
+                    }
+                }
+        }, "小女").start();
 
         for (int i = 0; i <= 5; i++) {
             new Thread(() -> {
@@ -40,9 +62,10 @@ public class Test07 {
         Sleeper(1);
         new Thread(() -> {
             synchronized (room) {
-                hasCigarette = true;
-                log.debug("烟送到了");
-                room.notify();
+                hasTakeout = true;
+                log.debug("外卖送到了");
+//                room.notify();
+                room.notifyAll();
             }
         }, "送烟").start();
     }
