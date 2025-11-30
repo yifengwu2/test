40726eb0f89aadea6646a81e244cfd4386290ef3
yifengwu2 <2454495978@qq.com>
2025-11-30 20:29:31 +0800

    测试wait送烟

---
+++ Test07.java
@@ -0,0 +1,50 @@
+package com.juc;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Test07 {
+    static final Object room = new Object();
+    static boolean hasCigarette = false;
+    static boolean hasTakeout = false;
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            synchronized (room) {
+                log.debug("有烟没[{}]", hasCigarette);
+                if (!hasCigarette) {
+                    log.debug("没烟休息会");
+                    Sleeper(2);
+                }
+                log.debug("有烟没[{}]", hasCigarette);
+                if (hasCigarette) {
+                    log.debug("小南开始干活");
+                }
+            }
+        }, "小南");
+        t1.start();
+
+        for (int i = 0; i <= 5; i++) {
+            new Thread(() -> {
+                synchronized (room) {
+                    log.debug("可以开始干活");
+                }
+
+            }, "其他人").start();
+        }
+        Sleeper(1);
+        new Thread(() -> {
+            hasCigarette = true;
+            log.debug("烟送到了");
+        }, "送烟").start();
+    }
+
+    private static void Sleeper(int i) {
+        try {
+            Thread.sleep(i * 1000L);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
